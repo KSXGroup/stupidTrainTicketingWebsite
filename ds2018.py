1c78311445aaa80e08d5561ffd2b43d4c806413f
KSXGroup <kstarxin@gmail.com>
2018-06-11 02:06:49 +0800

    almost fin

--- ds2018.py
+++ ds2018.py
@@ -173,7 +173,7 @@ def signin():
                 session['home_err_info'] = 'Login failed! User does not exist.'
             else:
                 session['user_name'] = reply.split(' ')[0]
-            return redirect(url_for('home'))
+            return redirect(url_for('userZone'))
     else:
         return render_template('signin.html')
 
@@ -186,20 +186,22 @@ def signup():
         password = request.form['password']
         repassword = request.form['repassword']
         if password != repassword:
-            return render_template('signup.html', err_info='The two passwords you typed do not match.')
+            return json.dumps("0")
         reply = db_communicate(' '.join(['register', name, password, email, phone]))
-        if reply == '-1':
-            return render_template('signup.html', err_info='Registration failed for some reason.')
+        if int(reply) < 2018:
+            return json.dumps("0")
         else:
-            userid = int(reply)
-            session['home_success_info'] = 'Registration successful! Your account ID is %s.' % (userid)
-            return redirect(url_for('home'))
+            userid = int(reply);
+            return json.dumps(str(userid))
     else:
-        if 'user_id' in session and 'user_name' in session and session['user_name'] != '':
+        if 'user_id' in session and 'user_name' in session and session['user_name'] != None and session['user_name'] != "" and session['user_id'] != None and session['user_id'] != "":
             user_name = session['user_name']
+            user_name = session['user_id']
+            return redirect(url_for('userZone'))
         else:
             user_name = None
-        return render_template('signup.html', user_name = user_name)
+            user_id = None
+            return render_template('signup.html')
 
 @app.route('/signout', methods=['GET'])
 def signout():
@@ -208,50 +210,6 @@ def signout():
     if 'user_name' in session:
         session.pop('user_name', None)
     return redirect(url_for('home'))
-
-"""
-@app.route('/profile', methods=['GET'])
-def profile():
-    if 'user_id' not in session:
-        session['home_err_info'] = 'Error! You have not signed in.'
-        return redirect(url_for('home'))
-    user_id = session['user_id']
-    reply = db_communicate(' '.join(['query_profile', user_id]))
-    if reply == '0':
-        session['home_err_info'] = 'Error! User does not exist.'
-        return redirect(url_for('home'))
-    else:
-        vec = reply.split(' ')
-        user_name = vec[0]
-        user_email = vec[1]
-        user_phone = vec[2]
-        user_priv = vec[3]
-    return render_template('profile.html', user_id=user_id, user_name=user_name, user_email=user_email, user_phone=user_phone, user_priv=user_priv)
-
-@app.route('/settings', methods=['POST', 'GET'])
-def settings():
-    if 'user_id' not in session:
-        session['home_err_info'] = 'Error! You have not signed in.'
-        return redirect(url_for('home'))
-    if request.method == 'POST':
-        user_id = session['user_id']
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        password = request.form['password']
-        repassword = request.form['repassword']
-        if password != repassword:
-            return render_template('settings.html', err_info='The two passwords you typed do not match.')
-        reply = db_communicate(' '.join(['modify_profile', user_id, name, password, email, phone]))
-        if reply == '0':
-            return render_template('settings.html', err_info='Modification failed for some reason.')
-        else:
-            session['home_success_info'] = 'Profile modified successfully!'
-            return redirect(url_for('home'))
-    else:
-        user_name = session['user_name']
-        return render_template('settings.html', user_name=user_name)
-"""
 
 @app.route('/orderTic', methods=['POST', 'GET'])
 def orderTic():
